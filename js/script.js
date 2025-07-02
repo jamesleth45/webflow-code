6af5bda9df08c3320066ca125bf7f62a2d01e147
James Lethbridge <jameslethbridge@Jamess-iMac.Home>
2025-07-02 19:28:20 +0100

    Update CSS styles

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 // #region Webflow Class Cleanup
 document.addEventListener('DOMContentLoaded', () => {
-  const classesToRemove = ['w-inline-block', 'w--current'];
+  const classesToRemove = ['w-inline-block', 'w-embed'];
 
   classesToRemove.forEach(className => {
     document.querySelectorAll(`.${className}`).forEach(el => {
       el.classList.remove(className);
+    });
+  });
+
+  requestAnimationFrame(() => {
+    document.querySelectorAll('.w--current').forEach(el => {
+      el.classList.remove('w--current');
     });
   });
 });
