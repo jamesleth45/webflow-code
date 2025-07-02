b171dae85d2e07bccc023ee0f814b540af0e4ad7
James Lethbridge <jameslethbridge@Jamess-iMac.Home>
2025-07-02 19:33:44 +0100

    Update CSS styles

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 // #region Webflow Class Cleanup
 document.addEventListener('DOMContentLoaded', () => {
-  const classesToRemove = ['w-inline-block', 'w-embed'];
+  const classesToRemove = [
+    'w-inline-block',
+    'w-embed'
+  ];
 
   classesToRemove.forEach(className => {
     document.querySelectorAll(`.${className}`).forEach(el => {
@@ -12,6 +15,26 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('.w--current').forEach(el => {
       el.classList.remove('w--current');
     });
+  });
+});
+// #endregion
+
+// #region Replace Divs with Spans
+document.addEventListener('DOMContentLoaded', () => {
+  const classNames = [
+    'header__logo-text',
+    'header__nav-text',
+    'footer__nav-text'
+  ];
+
+  classNames.forEach(className => {
+    const el = document.querySelector(`.${className}`);
+    if (el?.tagName === 'DIV') {
+      const span = document.createElement('span');
+      span.className = el.className;
+      span.innerHTML = el.innerHTML;
+      el.replaceWith(span);
+    }
   });
 });
 // #endregion
