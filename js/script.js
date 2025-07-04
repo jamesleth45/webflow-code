d8ba4ee320fdf6b6cdb180d5f29b9bfd17172fc7
James Lethbridge <jameslethbridge@Jamess-iMac.Home>
2025-07-04 10:54:17 +0100

    Update CSS styles

--- js/script.js
+++ js/script.js
@@ -141,7 +141,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const nav = document.querySelector('.header__nav');
   const btnSearch = document.querySelector('.header__mobile-btn--search');
   const btnBag = document.querySelector('.header__mobile-btn--bag');
-  const iconHam = document.querySelector('.header__mobile-icon--ham');
+  const iconHam = document.querySelector('.header__mobile-icon--cart');
   const iconX = document.querySelector('.header__mobile-icon--x');
 
   iconX?.setAttribute('data-visible', 'false'); // Ensure X is hidden on load
