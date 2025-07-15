a05d761959df2901d3231fb6e88ce4dda05b2083
James Lethbridge <jameslethbridge@Jamess-iMac.Home>
2025-07-15 22:47:40 +0100

    Update CSS styles

--- js/script.js
+++ js/script.js
@@ -250,7 +250,7 @@ document.addEventListener('DOMContentLoaded', () => {
             class="form__checkbox"
           >
           <span class="form__checkbox-text">
-            I have read and understood the Privacy Policy
+            Subscribe to receive information about James Carter shows, offers, news and events.
           </span>
         </label>
       </div>
